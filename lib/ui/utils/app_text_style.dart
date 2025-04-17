123d32cf475e74b742286efa16191b2bee88ecbf
abdelmonam <abdelmonamamr21@gmail.com>
2025-04-17 20:14:59 +0200

    + app stucture. + add colors. + add style.

---
+++ lib/ui/utils/app_text_style.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:islamy/ui/utils/app_colors.dart';
+
+abstract class AppTextStyle {
+  static const gold16w400 = TextStyle(
+      color: AppColors.gold, fontSize: 16, fontWeight: FontWeight.w400);
+  static const gold24w700 = TextStyle(
+      color: AppColors.gold, fontSize: 24, fontWeight: FontWeight.w700);
+  static const gold20w700 = TextStyle(
+      color: AppColors.gold, fontSize: 20, fontWeight: FontWeight.w700);
+  static const gold16w700 = TextStyle(
+      color: AppColors.gold, fontSize: 16, fontWeight: FontWeight.w700);
+  static const white16w700 = TextStyle(
+      color: AppColors.white, fontSize: 16, fontWeight: FontWeight.w700);
+  static const white36w700 = TextStyle(
+      color: AppColors.white, fontSize: 36, fontWeight: FontWeight.w700);
+  static const white32w700 = TextStyle(
+      color: AppColors.white, fontSize: 32, fontWeight: FontWeight.w700);
+  static const white20w700 = TextStyle(
+      color: AppColors.white, fontSize: 20, fontWeight: FontWeight.w700);
+  static const white12w700 = TextStyle(
+      color: AppColors.white, fontSize: 12, fontWeight: FontWeight.w700);
+  static const black24w700 = TextStyle(
+      color: AppColors.black, fontSize: 24, fontWeight: FontWeight.w700);
+  static const black14w700 = TextStyle(
+      color: AppColors.black, fontSize: 14, fontWeight: FontWeight.w700);
+  static const black16w700 = TextStyle(
+      color: AppColors.black, fontSize: 16, fontWeight: FontWeight.w700);
+  static const black20w700 = TextStyle(
+      color: AppColors.black, fontSize: 20, fontWeight: FontWeight.w700);
+}
